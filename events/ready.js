dd3feae92b396d412ed448547147cf936f4242fb
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-18 00:11:00 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -23,7 +23,7 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 setInterval(() => client.user.setActivity({ 
   name: `/help │ /owner`, 
-  type: ActivityType.Sex }), 10000);
+  type: ActivityType.Listening }), 10000);
 
 client.errorLog = config.errorLog
 } else {
