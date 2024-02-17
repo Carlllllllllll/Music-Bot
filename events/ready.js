2512b56dc214a0c7a3b3d447036f7bf3fbb77712
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-17 23:47:44 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -22,8 +22,8 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `Update v3.5`, 
-  type: ActivityType.Watching }), 10000);
+  name: `/𝐡𝐞𝐥𝐩 │ /𝐨𝐰𝐧𝐞𝐫`, 
+  type: ActivityType.LISTENING }), 10000);
 client.errorLog = config.errorLog
 } else {
 console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
