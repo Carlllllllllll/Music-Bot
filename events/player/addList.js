14c4c4f92809b506fb97d69025ecf6422baeeff7
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-19 22:48:01 +0200

    Update addList.js

--- events/player/addList.js
+++ events/player/addList.js
@@ -4,8 +4,7 @@ const db = require("../../mongoDB");
 module.exports = async (client, queue, playlist) => {
     try {
         const embed = new MessageEmbed()
-            .setDescription(`Hello 🛠️ Please bear with us as the bot is still in development. This is the first version, and this command is on the future features list. No worries, the owner is currently updating me. I will send a message when this command has been completed. 🚀`)
-            .addField(`${playlist.user.id}`, `\`${playlist.name} (${playlist.songs.length} 
+            .setDescription(`Hello 🛠️ Please bear with us as the bot is still in development. This is the first version, and this command is on the future features list. No worries, the owner is currently updating me. I will send a message when this command has been completed. 🚀\n\n<@${playlist.user.id}>, \`${playlist.name} (${playlist.songs.length} ❌)\` ❌❌`)
             .setImage("https://media1.tenor.com/m/B7_W48WYpuoAAAAC/10.gif");
 
         const sentMessage = await queue.textChannel.send({ embeds: [embed] });
