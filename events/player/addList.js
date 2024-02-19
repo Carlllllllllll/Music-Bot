75cc0ae5dd3d654c47b1436b2ea30030eae7ab7e
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-19 22:44:12 +0200

    Update addList.js

--- events/player/addList.js
+++ events/player/addList.js
@@ -5,7 +5,7 @@ module.exports = async (client, queue, playlist) => {
     try {
         const embed = new MessageEmbed()
             .setDescription(`Hello 🛠️ Please bear with us as the bot is still in development. This is the first version, and this command is on the future features list. No worries, the owner is currently updating me. I will send a message when this command has been completed. 🚀`)
-            .addField(`${playlist.user.id}`, `\`${playlist.name} (${playlist.songs.length} ❌)\` ❌❌`)
+            .addField(`${playlist.user.id}`, `\`${playlist.name} (${playlist.songs.length} 
             .setImage("https://media1.tenor.com/m/B7_W48WYpuoAAAAC/10.gif");
 
         const sentMessage = await queue.textChannel.send({ embeds: [embed] });
