10da462537c28bd4860f0101acb0d4fbc59efc0c
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-19 22:05:41 +0200

    Update addList.js

--- events/player/addList.js
+++ events/player/addList.js
@@ -1,8 +1,10 @@
 const db = require("../../mongoDB");
 module.exports = async (client, queue, playlist) => {
 
-queue?.textChannel?.send({ content: `<@${playlist.user.id}>, \`${playlist.name} (${playlist.songs.length + " " + '❌'})\` ❌❌` }).catch(e => { })
-}
+    queue?.textChannel?.send({ 
+        content: `Hello 🛠️ Please bear with us as the bot is still in development. This is the first version, and this command is on the future features list. No worries, the owner is currently updating me. I will send a message when this command has been completed. 🚀\n\n<@${playlist.user.id}>, \`${playlist.name} (${playlist.songs.length + " " + '❌'})\` ❌❌` 
+    }).catch(e => { });
+};
 
 /*
 
