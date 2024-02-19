37b4aea3d309cfd0ab184dd479112bf486dacc9d
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-19 23:03:24 +0200

    Update addList.js

--- events/player/addList.js
+++ events/player/addList.js
@@ -1,20 +1,8 @@
-const { MessageEmbed } = require("discord.js");
 const db = require("../../mongoDB");
-
 module.exports = async (client, queue, playlist) => {
-    try {
-        const embed = new MessageEmbed()
-            .setDescription(`Hello 🛠️ Please bear with us as the bot is still in development. This is the first version, and this command is on the future features list. No worries, the owner is currently updating me. I will send a message when this command has been completed. 🚀\n\n<@${playlist.user.id}>, \`${playlist.name} (${playlist.songs.length} ❌)\` ❌❌`)
-            .setImage("https://media1.tenor.com/m/B7_W48WYpuoAAAAC/10.gif");
 
-        const sentMessage = await queue.textChannel.send({ embeds: [embed] });
-        console.log("Message Sent:", sentMessage.content); // Log the content of the sent message
-    } catch (error) {
-        console.error("Error:", error);
-    }
-};
-
-
+queue?.textChannel?.send({ content: `<@${playlist.user.id}>, \`${playlist.name} (${playlist.songs.length + " " + '❌'})\` ❌❌` }).catch(e => { })
+}
 /*
 
   ██████╗░████████╗██╗░░██╗           
