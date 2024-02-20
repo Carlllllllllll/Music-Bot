da1171618111c63598231e817b0dd7217f0a433a
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-20 22:00:01 +0200

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -19,7 +19,7 @@ module.exports = {
        .setDescription(`Hello! 🎵 I am Music Bot, and I have a deep love for music. Carl brought me to life, but as this is my first version, I might have a few bugs and quirks. No worries, though – Carl is here to help! 🤖✨ Join our support server for swift assistance and a musical journey together! 🎵🚀\nSupport Server: [Join Here](${supportServerLink})`)
        .setTimestamp()
        .setImage('https://media1.tenor.com/m/7GyHsInT8uoAAAAC/naruto.gif');
-        
+      .setFooter({ text: 'More info - Use /help command │ Made By 𝑪𝒂𝒓𝒍 ⚡' });
       interaction.reply({ embeds: [embed] }).catch(() => {});
     } catch (e) {
       console.error(e);
