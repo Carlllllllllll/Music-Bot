5eb1cf14ef463ba1baad7e5890d070a93a96c631
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-20 22:15:36 +0200

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -12,13 +12,13 @@ module.exports = {
       const supportServerLink = 'https://discord.gg/SZys86VkMQ';
 
       const embed = new EmbedBuilder()
-        .setColor('#00ff00')
+        .setColor('#ffff00')
         .setAuthor({
           name: 'Bot Info',
         })
-       .setDescription(`Hello! 🎵 I am Music Bot, and I have a deep love for music. Carl brought me to life, but as this is my first version, I might have a few bugs and quirks. No worries, though – Carl is here to help! 🤖✨ Join our support server for swift assistance and a musical journey together! 🎵🚀\nSupport Server: [Join Here](${supportServerLink})`)
+       .setDescription(`Hello! 🎵 I am Music Bot, and I have a deep love for music. Carl brought me to life, but as this is my first version, I might have a few bugs and quirks. No worries, though Carl is here to help! 🤖 ✨ Join our support server for swift assistance and a musical journey together! 🎵 🚀\nSupport Server: [Join Here](${supportServerLink})`)
        .setTimestamp()
-       .setImage('https://media1.tenor.com/m/7GyHsInT8uoAAAAC/naruto.gif');
+       .setImage('https://media1.tenor.com/m/WI5z7d934jIAAAAC/naruto-smile.gif');
       interaction.reply({ embeds: [embed] }).catch(() => {});
     } catch (e) {
       console.error(e);
