7a7a63e92a15dfacc3dcf05e2c781afb91293f79
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-20 22:06:48 +0200

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js');
 const db = require('../mongoDB');
 
 module.exports = {
-  name: 'bot info',
+  name: 'bot-info',
   description: 'Get information about the bot.',
   permissions: '0x0000000000000800',
   options: [],
