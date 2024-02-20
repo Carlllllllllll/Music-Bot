6efd0359f520834798e8dcaf9638b5ae840da034
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-20 22:16:06 +0200

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -16,7 +16,7 @@ module.exports = {
         .setAuthor({
           name: 'Bot Info',
         })
-       .setDescription(`Hello! 🎵 I am Music Bot, and I have a deep love for music. Carl brought me to life, but as this is my first version, I might have a few bugs and quirks. No worries, though Carl is here to help! 🤖 ✨ Join our support server for swift assistance and a musical journey together! 🎵 🚀\nSupport Server: [Join Here](${supportServerLink})`)
+       .setDescription(`Hello! 🎵 I am Music Bot, and I have a deep love for music. Carl brought me to life, but as this is my first version, I might have a few bugs and quirks. No worries, though Carl is here to help! 🤖 ✨ Join our support server for swift assistance and a musical journey together! 🎵 🚀 \nSupport Server: [Join Here](${supportServerLink})`)
        .setTimestamp()
        .setImage('https://media1.tenor.com/m/WI5z7d934jIAAAAC/naruto-smile.gif');
       interaction.reply({ embeds: [embed] }).catch(() => {});
