340633807f6d5c56fb7d9f714165bdfa7daaedfa
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-21 17:28:32 +0200

    Update playlist.js

--- commands/playlist.js
+++ commands/playlist.js
@@ -213,7 +213,7 @@ saveTime: Date.now()
 }
 }, { upsert: true }).catch(e => { })
 
-await interaction.editReply({ content: `<@${interaction.member.id}>, \`${res[0]?.name}\` Playing Now!` }).catch(e => { })
+await interaction.editReply({ content: `<@${interaction.member.id}>, \`${res[0]?.name}\` Song has been added successfully!` }).catch(e => { })
 
 }
 
