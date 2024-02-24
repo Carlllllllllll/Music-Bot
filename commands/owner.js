859f0fc88154c7807d1cb8851e5e28fc35cec430
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-24 11:44:55 +0200

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -33,7 +33,6 @@ module.exports = {
           name: 'Bot Owner',
         })
         .setDescription(`__**About me**__:\n\n ▶️ I'm Carl, a Discord bot developer and web developer. I enjoy playing games, watching anime, and building various web server applications. For faster replies, contact me on Discord!\n Support Server: [Join Here](${discordServerLink})\n Discord Profile: [My Discord Profile](${discordProfileLink})`)
-        .setTimestamp()
         .setImage('https://media1.tenor.com/m/7GyHsInT8uoAAAAC/naruto.gif');
         
       interaction.reply({ embeds: [embed] }).catch(() => {});
