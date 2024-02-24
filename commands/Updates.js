5a561ef14dcaacd6febb5c064b08ed980b45e670
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-24 11:43:21 +0200

    Update Updates.js

--- commands/Updates.js
+++ commands/Updates.js
@@ -10,10 +10,9 @@ module.exports = {
   run: async (client, interaction) => {
     try {
       const supportServerLink = 'https://discord.gg/CgBZthGTXQ';
-      const BotInviteLink = 'https://discord.com/oauth2/authorize?client_id=1200206489260933232&permissions=8&scope=bot';
 
       const embed = new EmbedBuilder()
-        .setColor('#ffff00')
+        .setColor('##00ff00')
         .setAuthor({
           name: 'Updates',
         })
@@ -31,7 +30,7 @@ module.exports = {
          
          **Do you have a problem with our bot?**
          
-         No worries! Join our support server and type /report; our support team is online 24/7.`)
+         No worries! Join our support server and type /report; our support team is online 24/7.\n Support Server: [Join Here](${discordServerLink})`)
         
       
        .setImage('https://media1.tenor.com/m/N6X3ceoYtUkAAAAC/never-update-chicken.gif');
