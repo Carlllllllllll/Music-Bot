9656096ff345e404e482840743ac811d89992299
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-24 12:23:00 +0200

    Update and rename test.js to up.js

--- commands/up.js
+++ commands/up.js
@@ -2,8 +2,8 @@ const { EmbedBuilder } = require('discord.js');
 const db = require('../mongoDB');
 
 module.exports = {
-  name: 'test',
-  description: 'Get information about the bot owner.',
+  name: 'updates',
+  description: 'Get the bot updates',
   permissions: '0x0000000000000800',
   options: [],
 
@@ -15,9 +15,9 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#00ff00')
         .setAuthor({
-          name: 'Bot Owner',
+          name: 'Updates',
         })
-        .setDescription(`**Upades**
+        .setDescription(`
        
        New commands have been added: /help-us, /bot-info, and the playlists commands has been successfully created. Type /help to see how to use all bot commands.
        
@@ -31,8 +31,8 @@ module.exports = {
          
          **Do you have a problem with our bot?**
          
-         No worries! Join our support server and type /report; our support team is online 24/7.)`)
-        .setImage('https://media1.tenor.com/m/7GyHsInT8uoAAAAC/naruto.gif');
+         No worries! Join our support server and type /report; our support team is online 24/7.\n Support Server: [Join Here](${discordServerLink})')
+        .setImage('https://media1.tenor.com/m/N6X3ceoYtUkAAAAC/never-update-chicken.gif');
         
       interaction.reply({ embeds: [embed] }).catch(() => {});
     } catch (e) {
