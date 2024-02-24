1dadb59c4f5ce954bbe60b45353436a60d8e3bbb
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-24 18:37:26 +0200

    Update welcome.js

--- events/welcome.js
+++ events/welcome.js
@@ -1,14 +1,16 @@
-const { MessageEmbed } = require('discord.js');
+const { Client } = require('discord.js');
+const myBot = new Client();
 
-client.on('guildCreate', (guild) => {
-    const embed = new MessageEmbed()
+// ... (other bot setup code)
+
+myBot.on('guildCreate', (guild) => {
+    const embedBuilder = new MessageEmbed()
         .setTitle('Bot Added to Server')
         .setDescription(`Thanks for adding the bot to ${guild.name}!`)
-        .setColor('#00ff00'); // You can customize the color
-
-    // Convert to EmbedBuilder
-    const embedBuilder = new MessageEmbed(embed);
+        .setColor('#00ff00');
 
     // Send the embedBuilder to a specific channel or use guild.owner.send(embedBuilder) to DM the server owner
     // Example: guild.channels.cache.find(channel => channel.name === 'general').send(embedBuilder);
 });
+
+// ... (other event listeners and bot login code)
