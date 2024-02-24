bf15409c7d23f6e86e2d0db1796329521c6852fb
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-24 12:44:30 +0200

    Create updates.js

---
+++ commands/updates.js
@@ -0,0 +1,36 @@
+const { EmbedBuilder } = require('discord.js');
+const db = require('../mongoDB');
+
+module.exports = {
+  name: 'updates',
+  description: 'Get information about the bot updates',
+  permissions: '0x0000000000000800',
+  options: [],
+
+  run: async (client, interaction) => {
+    try {
+      const discordServerLink = 'https://discord.gg/aQHhb2Sb';
+
+
+      const embed = new EmbedBuilder()
+        .setColor('#00ff00')
+        .setDescription(`
+          **Updates**
+          New commands have been added: /help-us, /bot-info, and the playlists commands have been successfully created. Type /help to see how to use all bot commands.
+
+          **Bug Fixed**
+          When you type /play and enter your song name, you will receive :x: No results found. **[FIXED]**
+
+          **Changes**
+          At this moment, there haven't been any changes.
+
+          **Do you have a problem with our bot?**
+          No worries! Join our support server and type /report our support team is always online. \n Support Server: [Join Here](${discordServerLink})`)
+        .setImage('https://media1.tenor.com/m/7GyHsInT8uoAAAAC/naruto.gif');
+        
+      interaction.reply({ embeds: [embed] }).catch(() => {});
+    } catch (e) {
+      console.error(e);
+    }
+  },
+};
