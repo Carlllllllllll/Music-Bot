7bd09516c185a35ebf6b881f9881b255a788abb6
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-24 12:29:27 +0200

    Update up.js

--- commands/up.js
+++ commands/up.js
@@ -14,26 +14,22 @@ module.exports = {
 
       const embed = new EmbedBuilder()
         .setColor('#00ff00')
-        .setAuthor({
-          name: 'Updates',
-        })
+        .setAuthor('Updates')
         .setDescription(`
-       
-       New commands have been added: /help-us, /bot-info, and the playlists commands has been successfully created. Type /help to see how to use all bot commands.
-       
-       **Bug Fixed**
-       
-         When you type /play and enter your song name, you will receive ❌ No results found. **[FIXED]**
-         
-         **Changes**
-         
-         At this moment, there haven't been any changes.
-         
-         **Do you have a problem with our bot?**
-         
-         No worries! Join our support server and type /report; our support team is online 24/7.\n Support Server: [Join Here](${discordServerLink})')
+          New commands have been added: /help-us, /bot-info, and the playlists commands have been successfully created. Type /help to see how to use all bot commands.
+
+          **Bug Fixed**
+          When you type /play and enter your song name, you will receive ❌ No results found. **[FIXED]**
+
+          **Changes**
+          At this moment, there haven't been any changes.
+
+          **Do you have a problem with our bot?**
+          No worries! Join our support server and type /report; our support team is online 24/7.
+          Support Server: [Join Here](${discordServerLink})
+        `)
         .setImage('https://media1.tenor.com/m/N6X3ceoYtUkAAAAC/never-update-chicken.gif');
-        
+
       interaction.reply({ embeds: [embed] }).catch(() => {});
     } catch (e) {
       console.error(e);
