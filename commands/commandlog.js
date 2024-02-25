9d2021122afd7e545a63bc41b74aff33aac6901d
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-25 17:16:34 +0200

    Update commandlog.js

--- commands/commandlog.js
+++ commands/commandlog.js
@@ -22,7 +22,7 @@ module.exports = {
       .addField({ name: "Guild Of Use", value: `\`${guild.name}\` (${guild.id})` })
       .addField({ name: "Channel Of Use", value: `\`${channel.name}\` (${channel.id})` })
       .addField({ name: "Command User", value: `\`${user.username}\` (${user.id})` })
-      .setFooter({'Interaction Use Logger'})
+      .setFooter('Interaction Use Logger')
       .setTimestamp();
 
     const button = new ButtonBuilder()
