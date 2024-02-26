1ad7cbf0a28d841adebd81095160b8f18b146365
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-26 17:17:32 +0200

    Update logs.js

--- commands/logs.js
+++ commands/logs.js
@@ -24,6 +24,40 @@ module.exports = {
       .addField({ name:"Command User", value: `\`${user.username}\` (${user.id})`})
       .setFooter({ text: "Interaction Use Logger"})
       .setTimestamp();
-    
+    const button = new ButtonBuilder()
+      .setStyle(ButtonStyle.danger)
+      .setCustomID(`generateInviteLog`)
+      .setLabel(`Generate Sever Invite Link`)
+      .setDisabled(false);
+
+    const buttons = new ActionRowBuilder()
+      .addComponents(
+        button
+      );
+
+    var msg = await sendChannel.send({ embeds: [embed], components: [buttons] });
+
+    var time = 300000;  // Adjusted time to a reasonable value
+    const collector = await msg.createMessageComponentCollector({
+      componentType: ComponentType.Button,
+      time
+    });
+
+    collector.on("collect", async i => {
+      if (i.customId == 'generateInviteLog') {
+         var invite = await channel.createInvite();
+        await i.Reply({ content: `Here is the invite to the guild for command use: https://discord.gg/${invite.code}`, ephemeral: true });
+      }    
+  });
+
+    collector.on('end', async () => {
+      button.setDisabled(true);
+      embed.setFooter({ text: "Interaction Use Logger -- time ended"});
+      // Using deferReply and editReply instead of reply
+      await interaction.msg.edit({ embeds: [embed], components: [buttons] });
+   });
   }
 };
+     
+    
+    
