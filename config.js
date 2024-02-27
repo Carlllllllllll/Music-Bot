c47a1f311d8f1d404157cee3fd4252d2fba7d9d6
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-27 22:03:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -51,18 +51,3 @@ module.exports = {
 
   }
 }
-module.exports = {
-    client: {
-        token: '', // ← Your bot token (.env IS RECOMMENDED)
-        id: '1200206489260933232' // ← Your bot ID
-    },
-    modmail: {
-        guildId: '1145935264171180144', // ← Your server ID
-        categoryId: '1212084660042211459', // ← The modmail category ID
-        staffRoles: ['1187856441973952603'], // ← The modmail staff roles IDs
-        mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
-    },
-    logs: {
-        webhookURL: '' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
-    }
-};
