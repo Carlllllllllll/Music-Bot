86da9e9768f2ba028668ef15a54e00c60bcc1132
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-01 12:36:46 +0200

    Update suggestions.js

--- commands/suggestions.js
+++ commands/suggestions.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const Suggestion = require('RTX-MUSIC-BOT-v3.5/Schemas
-/suggestionSchema.js');
+const Suggestion = require('RTX-MUSIC-BOT-v3.5/Schemas/suggestionSchema.js');
 
 const CHANNEL_ID = 'Your_Channel_id';
 
