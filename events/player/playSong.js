c7a3d2c3f805721ef40bf9dcbf1d1e6d11ef4702
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-03-02 16:27:43 +0530

    playSong.js

--- events/player/playSong.js
+++ events/player/playSong.js
@@ -8,13 +8,13 @@ module.exports = async (client, queue, song) => {
       const embed = new EmbedBuilder()
       .setAuthor({
         name: 'Currently playing a Track',
-        iconURL: 'https://cdn.discordapp.com/attachments/1140841446228897932/1144671132948103208/giphy.gif', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        iconURL: 'https://cdn.discordapp.com/attachments/1189438802205343824/1210865677330681886/standard.gif?ex=65f5580a&is=65e2e30a&hm=18f64811d1e2144f17f66cabb7f49717d13b65771099aa8951253203fe486c86&', 
+        url: 'https://discord.com/invite/7U6yTNf8Vu'
     })
     .setDescription(`\n ‎ \n▶️ **Details :** **${song?.name}**\n▶️ **Enjoy the Ultimate Music Experience. ** \n▶️ **If link breaks playback try to give query.**`)
 .setImage(queue.songs[0].thumbnail)
     .setColor('#FF0000')
-    .setFooter({ text: 'More info - Use /help command [RTX GAMING]' });
+    .setFooter({ text: 'Dev By ZyX [/help]' });
      
       queue?.textChannel?.send({ embeds: [embed] }).catch(e => { });
     }
