798f54e5e26a3099d3c366e262fa78cd7730d2e4
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-03 22:45:40 +0200

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,5 +1,6 @@
 const { EmbedBuilder } = require('discord.js');
 const db = require('../mongoDB');
+const moment = require('moment');
 
 module.exports = {
   name: 'bot-info',
@@ -11,21 +12,32 @@ module.exports = {
     try {
       const supportServerLink = 'https://discord.gg/XuvB8URUez';
 
+      const uptimeStart = moment('2024-03-03T10:45:00'); // Set the start time of your bot
+
+      const currentTime = moment();
+      const uptimeDuration = moment.duration(currentTime.diff(uptimeStart));
+
+      const days = uptimeDuration.days();
+      const hours = uptimeDuration.hours();
+      const minutes = uptimeDuration.minutes();
+      const seconds = uptimeDuration.seconds();
+
       const embed = new EmbedBuilder()
         .setColor('#ffff00')
         .setAuthor({
           name: 'Bot Info',
         })
-       .setDescription(`Bot Developers: Carl (Owner)
+        .setDescription(`Bot Developers: **Carl (Owner)**
 Bot Version: **1.2.0**
 Bot System: **Music System**
 This bot was created on **01/16/2024**.
-Bot Status: Online since **03/03/2024**, since the last reboot.
+Bot Status: Online since **03/03/2024** **Since the last reboot**.
+Uptime: ${days}d ${hours}h ${minutes}m ${seconds}s
 Commands Count: **30**
-Commands Status : **Wokring**
+Commands Status: **Working**
 `)
-        
-       .setTimestamp();
+        .setTimestamp();
+
       interaction.reply({ embeds: [embed] }).catch(() => {});
     } catch (e) {
       console.error(e);
