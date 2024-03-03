24ff4f4a7202ce260d0b233e484a335ec1a53418
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-03 23:15:38 +0200

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -18,8 +18,8 @@ module.exports = {
           name: 'Supportr',
         })
         .setDescription(`Need help? No worries, we are here.\n
-Support Server: [Join Here](${discordServerLink})\n
-Developer Profile: [Carl (Owner)](${discordProfileLink})`)
+        Support Server: [Join Here](${discordServerLink})\n
+        Developer Profile: [Carl (Owner)](${discordProfileLink})`)
         .setImage('https://media1.tenor.com/m/MS32XqsnXAkAAAAC/friends-health.gif');
 
       interaction.reply({ embeds: [embed] }).catch(() => {});
