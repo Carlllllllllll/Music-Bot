b3cb5987b285e176b774e67d12dd234abc18adcd
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-04 15:06:04 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -22,7 +22,10 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `Music 🎶`, `To Your Vibe 🎶`
+  name: `Music 🎶`, 
+  type: ActivityType.Listening }), 10000);
+  setInterval(() => client.user.setActivity({ 
+  name: `To Your Vibe. 🎶`, 
   type: ActivityType.Listening }), 10000);
 
 client.errorLog = config.errorLog
