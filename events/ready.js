3cc8838cb2364d31bbad325635f08b4f37b91f60
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-04 16:22:20 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -23,13 +23,23 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 setInterval(() => client.user.setActivity({ 
   name: `Music 🎶`, 
-  type: ActivityType.Listening }), 10);
+  type: ActivityType.Listening }), 7000);
   setInterval(() => client.user.setActivity({ 
   name: `Your Vibe 🎶`, 
-  type: ActivityType.Listening }), 10);
+  type: ActivityType.Listening }), 7000);
   setInterval(() => client.user.setActivity({ 
   name: `Your Queue 🎶`, 
-  type: ActivityType.Watching }), 10);
+  type: ActivityType.Watching }), 7000);
+  setInterval(() => {
+  const serverCount = client.guilds.cache.size;
+  const memberCount = client.users.cache.size;
+
+  client.user.setActivity({
+    name: `Servers: ${serverCount} | Members: ${memberCount}`,
+    type: ActivityType.Watching
+  });
+}, 7000);
+
 
 
 
