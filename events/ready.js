9274d56dd8540e7bf0499fbc1f4f8d3f57701703
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-04 15:17:50 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -30,6 +30,20 @@ setInterval(() => client.user.setActivity({
   setInterval(() => client.user.setActivity({ 
   name: `Your Queue 🎶`, 
   type: ActivityType.Watching }), 10000);
+  setInterval(() => client.user.setActivity({ 
+  name: `Music 🎶`, 
+  type: ActivityType.Playing }), 10000);
+  setInterval(() => {
+  const serverCount = client.guilds.cache.size;
+  const memberCount = client.users.cache.size;
+
+  client.user.setActivity({
+    name: `Servers: ${serverCount} | Members: ${memberCount}`,
+    type: ActivityType.Watching
+  });
+}, 10000);
+
+
 
 client.errorLog = config.errorLog
 } else {
