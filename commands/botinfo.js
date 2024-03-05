815348ad5b808ed939543e602d0228a836d1baae
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-05 16:57:04 +0200

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -11,24 +11,38 @@ module.exports = {
     try {
       const supportServerLink = 'https://discord.gg/XuvB8URUez';
 
+      const botCreationDate = new Date('01/26/2024');
+      const lastRebootTime = new Date('03/03/2024');
+
+      const currentTime = new Date();
+      const uptimeSinceLastReboot = currentTime - lastRebootTime;
+
       const embed = new EmbedBuilder()
         .setColor('#11e008')
         .setAuthor({
           name: 'Bot Info',
         })
-       .setDescription(`Bot Developers: **Carl (Owner)**
+        .setDescription(`Bot Developers: **Carl (Owner)**
 Bot Version: **1.2.0**
 Bot System: **Music System**
-This bot was created on **01/26/2024**
-Bot Status: Online since **03/03/2024** **Since the last reboot**
+This bot was created on **${botCreationDate.toLocaleDateString()}**
+Bot Status: Online since the last reboot (**${formatUptime(uptimeSinceLastReboot)}**)
 Commands Count: **30**
 Commands Status : **Working**
 `)
-        
-       .setTimestamp();
+        .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(() => {});
     } catch (e) {
       console.error(e);
     }
   },
 };
+
+function formatUptime(uptime) {
+  const seconds = Math.floor((uptime / 1000) % 60);
+  const minutes = Math.floor((uptime / (1000 * 60)) % 60);
+  const hours = Math.floor((uptime / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(uptime / (1000 * 60 * 60 * 24));
+
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
