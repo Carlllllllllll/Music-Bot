5dde691b80ebce7a6101003998855ae7258bee0d
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-09 01:07:14 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -34,7 +34,7 @@ module.exports = {
           { name: '🔧 Updates ', value: 'To know about the bot updates' },
           { name: '🤖 Support ', value: 'To get our support links' },
           { name: '🎵 Recommended Playlists ', value: 'Some playlists will let you dance' },
-          { name: '👑 About Owner', value: 'Hello, I am Carl. I am here to help 24/7. If there are any bugs or if you have any questions, do not hesitate to DM me. For more information about me, type /about-owner in any channel.' }
+          { name: '👑 About Owner', value: 'To know info about the bot owner' }
         )
         .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=654f37ba&is=653cc2ba&hm=648a2e070fab36155f4171962e9c3bcef94857aca3987a181634837231500177`);
 
