94e6b38ed248845c7cecd57d46a59c2882276f9f
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-09 01:02:11 +0200

    Update pfp.js

--- commands/pfp.js
+++ commands/pfp.js
@@ -7,15 +7,15 @@ module.exports = {
   options: [], 
   run: async (client, interaction) => {
     try {
-
+    
       if (!interaction.member.permissions.has("ADMINISTRATOR")) {
         return interaction.reply({ content: "You don't have permission to use this command.", ephemeral: true });
       }
 
+      
+      const newPfpUrl = "https://www.bing.com/th/id/OGC.33a46f727dbe790d436616a1f56fce9c?pid=1.7&rurl=https%3a%2f%2fc.tenor.com%2flhlDEs5fNNEAAAAC%2fmusic-beat.gif&ehk=Q6VQ%2fljCF8E5c4SoMKS7WO0JVmawKSZeWjZRvoKZ7CY%3d";
 
-      const newPfpUrl = "https://th.bing.com/th?id=OIP.M6Rvcn2-eQ1DZhah9W_OnAHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2";
-
-
+     
       await client.user.setAvatar(newPfpUrl);
 
       // Create an embed to send a confirmation message
@@ -25,7 +25,7 @@ module.exports = {
         .setDescription("The bot's profile picture has been successfully updated.")
         .setImage(newPfpUrl);
 
-
+    
       await interaction.reply({ embeds: [embed], ephemeral: true });
     } catch (error) {
       console.error("Error updating profile picture:", error);
