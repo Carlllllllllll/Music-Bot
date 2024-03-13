1d7d2ad4ee458abe82b3a304491cfc17482e8b5c
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-13 23:08:28 +0200

    Update wl.js

--- events/wl.js
+++ events/wl.js
@@ -1,5 +1,5 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.Guilds] });
+const client = new Client({ intents: [Intents.Guilds] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
