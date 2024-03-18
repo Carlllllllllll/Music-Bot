4ab6db89b14d242b506f6b0edc1c0eca1c3a5ac6
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-18 20:26:06 +0200

    Update pfp.js

--- commands/pfp.js
+++ commands/pfp.js
@@ -1,3 +1,10 @@
+  // Import necessary modules from Discord.js
+const { Client, Routes, DataResolver } = require('discord.js');
+
+// Create a new Discord client
+const client = new Client();
+
+// Function to update the banner
 async function updateBanner() {
   try {
     const bannerUrl = "https://th.bing.com/th/id/R.fc4f45f89d3de367b4601a467cc9c166?rik=r8AiECSlQ%2ftWLw&riu=http%3a%2f%2fwww.galesaur.com%2fmusic%2f00-10.gif&ehk=3scZcznrIZpQeYBEZfWL69uR7A3SREZbsD7jOFswdSM%3d&risl=&pid=ImgRaw&r=0";
@@ -7,11 +14,14 @@ async function updateBanner() {
       body: { banner: resolvedBanner },
     });
 
-    console.log('the banner was uploaded successfully!!');
+    console.log('The banner was uploaded successfully!!');
   } catch (error) {
-    console.error('error when uploading the banner.', error);
+    console.error('Error when uploading the banner:', error);
   }
 }
 
+// Log in to Discord and execute the updateBanner function
+client.login('TOKEN').then(() => {
+  updateBanner();
+});
 
-updateBanner();
