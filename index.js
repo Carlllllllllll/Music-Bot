61b6ad90a2f63c8666a1cb73bacc0f29b0316b9e
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-03-18 21:01:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ manager.spawn();
 require("./bot.js")
 
 }
+const { Routes, DataResolver } = require('discord.js');
+// If you have client logged in or use REST
+await client.rest.patch(Routes.user(), {
+  body: { banner: await DataResolver.resolveImage("https://th.bing.com/th/id/R.fc4f45f89d3de367b4601a467cc9c166?rik=r8AiECSlQ%2ftWLw&riu=http%3a%2f%2fwww.galesaur.com%2fmusic%2f00-10.gif&ehk=3scZcznrIZpQeYBEZfWL69uR7A3SREZbsD7jOFswdSM%3d&risl=&pid=ImgRaw&r=0") },
 /*
 
   ██████╗░████████╗██╗░░██╗           
