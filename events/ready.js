d48b4d12fe3044cfef54694e2fcd117525b40c1b
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-04-12 17:23:34 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,62 +1,57 @@
 const config = require("../config.js");
-const { ActivityType, DataResolver } = require("discord.js");
-
+const { ActivityType  } = require("discord.js")
 module.exports = async (client) => {
-  if (config.mongodbURL || process.env.MONGO) {
-    const { REST } = require("@discordjs/rest");
-    const { Routes } = require("discord-api-types/v10");
-    const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
-    
-    (async () => {
-      try {
-        await rest.put(Routes.applicationCommands(client.user.id), {
-          body: await client.commands,
-        });
-        console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded!')
-      } catch (err) {
-        console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
-      }
-    })();
 
-    console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
-    setInterval(() => client.user.setActivity({ 
-      name: `Music 🎶`, 
-      type: ActivityType.Listening }), 4000);
-    setInterval(() => client.user.setActivity({ 
-      name: `Your Vibe 🎶`, 
-      type: ActivityType.Listening }), 4000);
-    setInterval(() => client.user.setActivity({ 
-      name: `Your Queue 🎶`, 
-      type: ActivityType.Watching }), 4000);
-    
+if (config.mongodbURL || process.env.MONGO) {
+
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
+(async () => {
+try {
+await rest.put(Routes.applicationCommands(client.user.id), {
+body: await client.commands,
+});
+console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded!')
+} catch (err) {
+console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
+}
+})();
+
+console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
+
+setInterval(() => client.user.setActivity({ 
+  name: `Music 🎶`, 
+  type: ActivityType.Listening }), 4000);
+  setInterval(() => client.user.setActivity({ 
+  name: `Your Vibe 🎶`, 
+  type: ActivityType.Listening }), 4000);
+  setInterval(() => client.user.setActivity({ 
+  name: `Your Queue 🎶`, 
+  type: ActivityType.Watching }), 4000);
     setInterval(() => {
-      const serverCount = client.guilds.cache.size;
-      const memberCount = client.users.cache.size;
+  const serverCount = client.guilds.cache.size;
+  const memberCount = client.users.cache.size;
 
-      client.user.setActivity({
-        name: `Servers: ${serverCount} | Members: ${memberCount}`,
-        type: ActivityType.Watching
-      });
-    }, 4000);
+  client.user.setActivity({
+    name: `Servers: ${serverCount} | Members: ${memberCount}`,
+    type: ActivityType.Watching
+  });
+}, 4000);
 
-    client.errorLog = config.errorLog;
+client.errorLog = config.errorLog
+} else {
+console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
+}
+console.log('\x1b[36m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
 
-    // Add your new code here
-    await client.rest.patch(Routes.user(), {
-      body: { banner: await DataResolver.resolveImage("https://www.gifcen.com/wp-content/uploads/2022/08/luffy-gif-4.gif") },
-    });
 
-  } else {
-    console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`);
-  }
-  console.log('\x1b[36m%s\x1b[0m', `|    🎯 Activity successfully set!`);
+if(client.config.voteManager.status === true && client.config.voteManager.api_key){
+const { AutoPoster } = require('topgg-autoposter')
+const ap = AutoPoster(client.config.voteManager.api_key, client)
+ap.on('posted', () => {
+})
+}
 
-  if (client.config.voteManager.status === true && client.config.voteManager.api_key) {
-    const { AutoPoster } = require('topgg-autoposter');
-    const ap = AutoPoster(client.config.voteManager.api_key, client);
-    ap.on('posted', () => {
-      // Handle event if needed
-    });
-  }
 }
