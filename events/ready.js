c4280d9271b92e78f4148b0683a69ec28c0f0563
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-04-12 17:20:13 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,12 @@
-const { REST, Routes, DataResolver } = require("discord.js");
+const config = require("../config.js");
+const { ActivityType, DataResolver } = require("discord.js");
 
 module.exports = async (client) => {
-  const config = require("../config.js");
-  const { ActivityType } = require("discord.js");
-
   if (config.mongodbURL || process.env.MONGO) {
     const { REST } = require("@discordjs/rest");
     const { Routes } = require("discord-api-types/v10");
     const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
-
+    
     (async () => {
       try {
         await rest.put(Routes.applicationCommands(client.user.id), {
@@ -31,6 +29,7 @@ module.exports = async (client) => {
     setInterval(() => client.user.setActivity({ 
       name: `Your Queue 🎶`, 
       type: ActivityType.Watching }), 4000);
+    
     setInterval(() => {
       const serverCount = client.guilds.cache.size;
       const memberCount = client.users.cache.size;
@@ -41,13 +40,13 @@ module.exports = async (client) => {
       });
     }, 4000);
 
-    // Add the new code block here
-    const rest = new REST().TOKEN("");
-    await rest.patch(Routes.user(), {
+    client.errorLog = config.errorLog;
+
+    // Add your new code here
+    await client.rest.patch(Routes.user(), {
       body: { banner: await DataResolver.resolveImage("https://www.gifcen.com/wp-content/uploads/2022/08/luffy-gif-4.gif") },
     });
 
-    client.errorLog = config.errorLog;
   } else {
     console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`);
   }
@@ -56,6 +55,8 @@ module.exports = async (client) => {
   if (client.config.voteManager.status === true && client.config.voteManager.api_key) {
     const { AutoPoster } = require('topgg-autoposter');
     const ap = AutoPoster(client.config.voteManager.api_key, client);
-    ap.on('posted', () => {});
+    ap.on('posted', () => {
+      // Handle event if needed
+    });
   }
-};
+}
