c3d63b86c4f018f6b828712d937bcb587cab1081
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-08-12 02:04:55 +0300

    Update pfp.js

--- commands/pfp.js
+++ commands/pfp.js
@@ -7,29 +7,28 @@ module.exports = {
   options: [], 
   run: async (client, interaction) => {
     try {
-    
       if (!interaction.member.permissions.has("ADMINISTRATOR")) {
         return interaction.reply({ content: "You don't have permission to use this command.", ephemeral: true });
       }
 
-      
+      // URL to the new profile picture
       const newPfpUrl = "https://media.discordapp.net/attachments/1208810080426795061/1271602484061671424/Gido-PFP-Carl.gif?ex=66b9e9d9&is=66b89859&hm=435b9550427e5f05bbff780e509e83170057b9f576f2380b672826c6b346c801&=";
 
-     
-      await client.user.setBanner(newPfpUrl);
+      // Set the bot's avatar (profile picture)
+      await client.user.setAvatar(newPfpUrl);
 
       // Create an embed to send a confirmation message
       const embed = new EmbedBuilder()
         .setColor("#00FF00")
-        .setTitle("Banner is now Updated")
-        .setDescription("The bot's banner has been successfully updated.")
+        .setTitle("Profile Picture Updated")
+        .setDescription("The bot's profile picture has been successfully updated.")
         .setImage(newPfpUrl);
 
-    
+      // Send the confirmation message
       await interaction.reply({ embeds: [embed], ephemeral: true });
     } catch (error) {
-      console.error("An error while adding the banner:", error);
-      await interaction.reply({ content: "An error occurred while updating the banner", ephemeral: true });
+      console.error("An error occurred while updating the profile picture:", error);
+      await interaction.reply({ content: "An error occurred while updating the profile picture.", ephemeral: true });
     }
   },
 };
