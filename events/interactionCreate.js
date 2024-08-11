243c67c0edb62033745bf927c3fb06baca0ded49
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-08-12 02:26:48 +0300

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,14 +4,17 @@ module.exports = async (client, interaction) => {
   if (!interaction.isCommand()) return;
 
   const command = client.commands.get(interaction.commandName);
-  if (!command) return;
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
 
   try {
-    const lang = loadLanguage; 
+    const lang = loadLanguage(); // Call loadLanguage as a function if it's not already
     await command.execute(interaction, client, lang);
   } catch (error) {
     console.error(error);
-    const lang = loadLanguage; 
+    const lang = loadLanguage(); // Call loadLanguage as a function if it's not already
     await interaction.reply({ content: lang.errorMessage || 'There was an error while executing this command!', ephemeral: true });
   }
 };
